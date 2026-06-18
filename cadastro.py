47fdca3c949ec342d232814c679c1f93512508fb
Matheus <117856458+matheusprojects@users.noreply.github.com>
2026-06-18 19:48:18 +0000

    feat curso

--- cadastro.py
+++ cadastro.py
@@ -1,3 +1,4 @@
 nome = input("Digite seu nome: ")
 sobrenome = str(input("Digite o sobrenome: "))
-print(f"{nome} {sobrenome}")
+curso = input("Digite seu curso: ")
+print(f"{nome} {sobrenome}" {curso})
