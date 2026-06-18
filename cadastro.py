594affdb71135cb2a6a502b2b00691b9826c5b5a
Matheus <117856458+matheusprojects@users.noreply.github.com>
2026-06-18 18:55:30 +0000

    feat: adiciona "cidade"

--- cadastro.py
+++ cadastro.py
@@ -1,3 +1,4 @@
 nome = input("Digite seu nome: ")
 so = str(input("Coloque o sobrenome: "))
 curso = input("Diga o nome do curso que você faz: ")
+cidade = input ("Diga qual o nome da cidade que você mora: ")
