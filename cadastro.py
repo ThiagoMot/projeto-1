8c208e72648a12f1991c4ef5a7bd67725c7346ba
Guilherme Martins de souza <160148068+Guilhermems081@users.noreply.github.com>
2026-06-19 02:33:54 +0000

    Desafio concluido

--- cadastro.py
+++ cadastro.py
@@ -1,10 +1,11 @@
 nome = input("Digite seu nome: ")
-sobrenome = str(input("Digite o sobrenome: "))
+sobrenome = input("Digite o sobrenome: ")
 curso = input("Digite seu curso: ")
 cidade = input("Digite sua cidade: ")
 idade = int(input("Digite sua idade: "))
-print(f"Aluno(a) {nome} {sobrenome} tem {idade} anos e faz o curso de {curso} e mora em {cidade}.")
-if idade >= 18:
-    print("O aluno é maior de idade.")
-else:
-    print("O aluno é menor de idade.")
+
+print("\n========== CADASTRO DO ALUNO ==========")
+print(f"Aluno(a): {nome} {sobrenome}")
+print(f"Idade: {idade} anos")
+print(f"Cidade: {cidade}")
+print(f"Curso: {curso}")
